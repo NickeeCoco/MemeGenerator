5d582417852c3a72d398f3763f583ae7c7555c4e
NickeeCoco <mo.bentzinger@gmail.com>
2020-03-04 15:25:34 -0500

    Updates

---
+++ precache-manifest.26a8cb80d3144b97f0bc2cf88cee4748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a37e114c56407f935c930d33e842d703",
+    "url": "/MemeGenerator/index.html"
+  },
+  {
+    "revision": "ef7fb48495c31f49bf11",
+    "url": "/MemeGenerator/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "97bd11b325e668e5ed35",
+    "url": "/MemeGenerator/static/js/2.515c42c4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/MemeGenerator/static/js/2.515c42c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef7fb48495c31f49bf11",
+    "url": "/MemeGenerator/static/js/main.a7fe3c52.chunk.js"
+  },
+  {
+    "revision": "76b3627b473e7f4a3636",
+    "url": "/MemeGenerator/static/js/runtime-main.53356ddb.js"
+  }
+]);
